fce0f868776897b3adb4f408ff47939d87277099
ishimwezachee <zacheeishimwe@gmail.com>
2021-11-16 16:10:54 +0200

    create external data and import them

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import scores from './scores.js';
+
+const list = document.querySelector('.container');
+
+scores.forEach((score) => {
+  list.innerHTML += `<li>${score.name}:${score.scores}</li>`;
+});
